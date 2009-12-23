cd67b571e539caec648fb213f1b41cef605623b3
Macha <macha.hack@gmail.com>
2009-12-23 19:55:17 +0000

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,22 @@
+This is a simple todo manager written in Python. At the moment it is command 
+line only, but a GUI version is being created.
+
+Usage
+=====
+*On Windows, use `python todo.py` wherever you see todo.py in this readme.*
+
+Download
+Install using `todo.py install`
+
+Adding Items
+-------------
+Use `todo.py add <text>`.
+
+Listing Items
+-------------
+Use `todo.py list`
+
+Removing Items
+--------------
+Use `todo.py remove <id>`
+To get the ID, use `todo.py list`
--- todo.py
+++ todo.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 
 import json
+import os
 import sys
+
+
+json_folder = os.path.expanduser("~") + "/.todopy/"
+json_location = json_folder + "todo.json"
 
 class Todo:
 
@@ -24,44 +29,39 @@ class Todo:
 				except IndexError:
 					print "Missing argument for command '" + sys.argv[1] + "' check help"
 
+	def install(self):
+		"""
+		Sets up the data files for storing the todo list.
+		"""
+		os.mkdir(json_folder)
+		json_file = open(json_location, "w")
+		json_file.write("[]")
+		json_file.close()
+
+		print "TodoPy Installed"
+
 	def add(self):
 		"""
 		Adds an item to the todo list.
 		"""
-		json_file = open("./todo.json", "r")
-		todo_list = json.loads(json_file.read())
-		json_file.close()
-
-		last_id = todo_list[-1]['id']
-
-		new_item = {'id': last_id + 1, 'text': ' '.join(sys.argv[2:])}
-
-		todo_list.append(new_item)
-
-		json_file = open("./todo.json", "w")
-		json_file.write(json.dumps(todo_list))
-		json_file.close()
+		text = ' '.join(sys.argv[2:])
+		todo_list = TodoList()
+		todo_list.add(text)
+		todo_list.save()
 
 		print
-		print "Added:", new_item['text']
+		print "Added:", text
 		print
 
 	def remove(self):
 		"""
 		Removes an item from the todo list by ID
 		"""
-		json_file = open("./todo.json", "r")
-		todo_list = json.loads(json_file.read())
-		json_file.close()
-
-		for todo in todo_list[:]:
-			if todo['id'] == int(sys.argv[2]):
-				removed_text = todo['text']
-				todo_list.remove(todo)
-
-		json_file = open("./todo.json", "w")
-		json_file.write(json.dumps(todo_list))
-		json_file.close()
+		id = int(sys.argv[2])
+		todo_list = TodoList()
+		removed_text = todo_list.remove(id)
+		todo_list.save()
+		
 
 		print
 		print "Removed", removed_text
@@ -71,15 +71,7 @@ class Todo:
 		"""
 		Prints out the todo list
 		"""
-		json_file = open("./todo.json", "r")
-		json_text = json_file.read()
-		json_file.close()
-		todo_list = json.loads(json_text)
-		
-		print
-		for todo in todo_list:
-			print todo['id'], "\t", todo['text']
-		print
+		print TodoList()
 
 	def help(self):
 		"""
@@ -90,6 +82,66 @@ class Todo:
 		print "\t add <message> \t- Adds a message to the todo list"
 		print "\t list \t\t- Lists all current todo items"
 		print "\t remove <ID> \t- Removes the todo item with the specified ID"
+		print "\t install \t- Sets up the data files needed to run the script"
 		print
+
+class TodoList:
+	
+	def __init__(self):
+		"""
+		Sets up the list.
+		"""
+		self.load()
+
+	def add(self, text):
+		"""
+		Adds an item to the list
+		"""
+		try:
+			last_id = self.list[-1]['id']
+		except IndexError:
+			# No items in list
+			last_id = 1
+
+		new_item = {'id': last_id + 1, 'text': text}
+		self.list.append(new_item)
+
+	def __str__(self):
+		string = ""
+		for item in self.list:
+			string = string + str(item['id']) + "\t" + str(item['text']) + "\n"
+		string = string[:-1] # Cut out final newline
+		return string
+			
+
+	def remove(self, id):
+		"""
+		Removes an item from the list.
+		"""
+		for todo in self.list[:]:
+			if todo['id'] == id:
+				removed_text = self.list['id']
+				self.list.remove(todo)
+				return removed_text
+
+		return "Nothing"
+
+	def load(self):
+		"""
+		Loads the list data from file.
+		"""
+		json_file = open(json_location, "r")
+		json_text = json_file.read()
+		json_file.close()
+		self.list = json.loads(json_text)
+
+	def save(self):
+		"""
+		Writes the list data to file.
+		"""
+		json_file = open(json_location, "w")
+		json_file.write(json.dumps(self.list))
+		json_file.close()
+
 
 Todo().run()
